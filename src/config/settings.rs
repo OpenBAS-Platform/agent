3ce062e227e12f729eefc7f6458592c33e12e3c5
Julien Richard <julien.richard@filigran.io>
2024-06-03 00:16:37 +0200

    [agent] Adapt config file for production

--- src/config/settings.rs
+++ src/config/settings.rs
@@ -3,6 +3,8 @@ use std::env;
 use log::info;
 use serde::Deserialize;
 
+const ENV_PRODUCTION: &str = "production";
+
 #[derive(Debug, Deserialize)]
 #[allow(unused)]
 pub struct OpenBAS {
@@ -19,13 +21,15 @@ pub struct Settings {
 
 impl Settings {
     pub fn new() -> Result<Self, ConfigError> {
-        let run_mode = env::var("env").unwrap_or_else(|_| "production".into());
+        let run_mode = env::var("env").unwrap_or_else(|_| ENV_PRODUCTION.into());
         info!("Starting OpenBAS agent 0.0.1 ({})", run_mode);
-        let s = Config::builder()
-            .add_source(File::with_name("config/default"))
-            .add_source(File::with_name(&format!("config/{}", run_mode)).required(false))
-            .add_source(Environment::with_prefix("openbas"))
-            .build()?;
-        s.try_deserialize()
+        let config = Config::builder().add_source(Environment::with_prefix("openbas"));
+        if run_mode == ENV_PRODUCTION {
+            config.add_source(File::with_name("openbas-agent").required(true)).build()?.try_deserialize()
+        } else {
+            config.add_source(File::with_name("config/default"))
+                .add_source(File::with_name(&format!("config/{}", run_mode)).required(false))
+                .build()?.try_deserialize()
+        }
     }
 }
