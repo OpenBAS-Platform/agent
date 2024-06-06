aaf820c98fe4600f5f74e29a7facc08202c95a40
Julien Richard <julien.richard@filigran.io>
2024-06-06 22:26:33 +0200

    [agent] Change executor to take care of changing the command path

--- src/process/agent_exec.rs
+++ src/process/agent_exec.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::process::{Command, Stdio};
 use base64::Engine;
 use base64::prelude::BASE64_STANDARD;
@@ -11,9 +12,12 @@ pub struct ExecutionResult {
 
 pub fn command_execution(command: &str) -> Result<ExecutionResult, Error> {
     if cfg!(target_os = "windows") {
-        let invoke_expression = format!("Invoke-Expression ([System.Text.Encoding]::UTF8.GetString([convert]::FromBase64String(\"{}\")))", BASE64_STANDARD.encode(command));
+        let current_exe_patch = env::current_exe().unwrap();
+        let executable_path = current_exe_patch.parent().unwrap();
+        let command_with_location = command.replace("#{location}", &executable_path.display().to_string().as_str());
+        info!("Invoking command execution {}", command_with_location);
+        let invoke_expression = format!("Invoke-Expression ([System.Text.Encoding]::UTF8.GetString([convert]::FromBase64String(\"{}\")))", BASE64_STANDARD.encode(command_with_location));
         let command_args = &["/d", "/c", "powershell.exe", "-ExecutionPolicy", "Bypass", "-WindowStyle", "Hidden", "-NonInteractive", "-NoProfile", "-Command", &invoke_expression];
-        info!("Invoking command execution {}", command_args.join(" "));
         let invoke_output = Command::new("cmd.exe")
             .args(command_args)
             .stderr(Stdio::piped())
@@ -24,6 +28,7 @@ pub fn command_execution(command: &str) -> Result<ExecutionResult, Error> {
         let stderr = String::from_utf8 (invoke_result.stderr).unwrap();
         Ok(ExecutionResult { stderr, stdout })
     } else {
+        // TODO implement linux execution
         // LINUX|MAC => /bin/sh -c "/bin/echo $1 | base64 -d | sh"
         Err(Error::Internal(String::from("Not implemented yet")))
     }
