1c994569d00e4498eedd9688de7b2ce5ccbdd356
Julien Richard <julien.richard@filigran.io>
2024-06-12 13:35:04 +0200

    [agent] Upgrade powershell scripts

--- installer/windows/agent-upgrade.ps1
+++ installer/windows/agent-upgrade.ps1
@@ -1,4 +1,1 @@
-Stop-Service -Force -Name \"OBAS Agent Service\"; Invoke-WebRequest -Uri " +
-              "\"${OPENBAS_URL}/api/agent/package/openbas/windows\" -OutFile \"openbas-installer.exe\"; " +
-              "./openbas-installer.exe /S ~OPENBAS_URL=\"${OPENBAS_URL}\" ~ACCESS_TOKEN=\"${OPENBAS_TOKEN}\"; " +
-              "Start-Sleep -Seconds 1.5; rm -force ./openbas-installer.exe;
+Stop-Service -Force -Name "OBAS Agent Service"; Invoke-WebRequest -Uri "${OPENBAS_URL}/api/agent/package/openbas/windows" -OutFile "openbas-installer.exe"; ./openbas-installer.exe /S ~OPENBAS_URL="${OPENBAS_URL}" ~ACCESS_TOKEN="${OPENBAS_TOKEN}"; Start-Sleep -Seconds 1.5; rm -force ./openbas-installer.exe;
