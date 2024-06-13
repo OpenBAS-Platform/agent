2db3dc9c17b6715750b935f58ee4743091945e98
Julien Richard <julien.richard@filigran.io>
2024-06-13 13:24:11 +0200

    [agent] script upgrade for windows

--- installer/windows/agent-installer.ps1
+++ installer/windows/agent-installer.ps1
@@ -1,1 +1,4 @@
-Stop-Service -Force -Name "OBAS Agent Service"; Invoke-WebRequest -Uri "${OPENBAS_URL}/api/agent/package/openbas/windows" -OutFile "openbas-installer.exe"; ./openbas-installer.exe /S ~OPENBAS_URL="${OPENBAS_URL}" ~ACCESS_TOKEN="${OPENBAS_TOKEN}"; Start-Sleep -Seconds 1.5; rm -force ./openbas-installer.exe;
+if ($env:PROCESSOR_ARCHITECTURE -eq "AMD64") { $architecture = "x86_64" }
+if ($env:PROCESSOR_ARCHITECTURE -eq "ARM64") { $architecture = "arm_64" }
+if ([string]::IsNullOrEmpty($architecture)) { throw "Architecture $env:PROCESSOR_ARCHITECTURE is not supported yet, please create a ticket in openbas github project" }
+Stop-Service -Force -Name "OBAS Agent Service"; Invoke-WebRequest -Uri "${OPENBAS_URL}/api/agent/package/openbas/windows/${architecture}" -OutFile "openbas-installer.exe"; ./openbas-installer.exe /S ~OPENBAS_URL="${OPENBAS_URL}" ~ACCESS_TOKEN="${OPENBAS_TOKEN}"; Start-Sleep -Seconds 1.5; rm -force ./openbas-installer.exe;
