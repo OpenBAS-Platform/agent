3f78c014e9f48d3f096240b2abffce2367dc3626
damgouj <damien.goujard@filigran.io>
2024-10-18 08:47:33 +0200

    [agent] Fix upgrade agent for Linux and Macos

--- installer/linux/agent-upgrade.sh
+++ installer/linux/agent-upgrade.sh
@@ -19,7 +19,18 @@ if [ "${os}" = "linux" ]; then
     mv /opt/openbas-agent/openbas-agent_upgrade /opt/openbas-agent/openbas-agent
     chmod 755 /opt/openbas-agent/openbas-agent
 
-    echo "02. Restarting the service"
+    echo "02. Updating OpenBAS configuration file"
+    cat > /opt/openbas-agent/openbas-agent-config.toml <<EOF
+debug=false
+
+[openbas]
+url = "${OPENBAS_URL}"
+token = "${OPENBAS_TOKEN}"
+unsecured_certificate = "${OPENBAS_UNSECURED_CERTIFICATE}"
+with_proxy = "${OPENBAS_WITH_PROXY}"
+EOF
+
+    echo "03. Restarting the service"
     systemctl restart openbas-agent || echo "Fail restarting openbas-agent"
 
     echo "OpenBAS Agent started."
--- installer/macos/agent-upgrade.sh
+++ installer/macos/agent-upgrade.sh
@@ -18,7 +18,18 @@ if [ "${os}" = "macos" ]; then
     mv /opt/openbas-agent/openbas-agent_upgrade /opt/openbas-agent/openbas-agent
     chmod 755 /opt/openbas-agent/openbas-agent
 
-    echo "02. Starting agent service"
+    echo "02. Updating OpenBAS configuration file"
+    cat > /opt/openbas-agent/openbas-agent-config.toml <<EOF
+debug=false
+
+[openbas]
+url = "${OPENBAS_URL}"
+token = "${OPENBAS_TOKEN}"
+unsecured_certificate = "${OPENBAS_UNSECURED_CERTIFICATE}"
+with_proxy = "${OPENBAS_WITH_PROXY}"
+EOF
+
+    echo "03. Starting agent service"
     launchctl bootout system/ ~/Library/LaunchDaemons/openbas-agent.plist || echo "openbas-agent already stopped"
     launchctl bootstrap system/ ~/Library/LaunchDaemons/openbas-agent.plist
 
