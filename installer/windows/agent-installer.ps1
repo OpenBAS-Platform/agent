a3130415d8ae8d511d600fd0583a29a198b92601
Damien Goujard <damien.goujard@filigran.io>
2025-01-16 11:37:08 +0100

    [agent] Improve installation script for Windows

--- installer/windows/agent-installer.ps1
+++ installer/windows/agent-installer.ps1
@@ -1,3 +1,8 @@
+$isElevatedPowershell = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if ($isElevatedPowershell -like "False") { throw "PowerShell 'Run as Administrator' is required for installation" }
+# Can't install the OpenBAS agent in System32 location because NSIS 64 exe
+$location = Get-Location
+if ($location -like "*C:\Windows\System32*") { cd C:\ }
 switch ($env:PROCESSOR_ARCHITECTURE)
 {
     "AMD64" {$architecture = "x86_64"; Break}
@@ -11,5 +16,17 @@ switch ($env:PROCESSOR_ARCHITECTURE)
 	}
 }
 if ([string]::IsNullOrEmpty($architecture)) { throw "Architecture $env:PROCESSOR_ARCHITECTURE is not supported yet, please create a ticket in openbas github project" }
-if ((Get-Host).Version.Major -lt 7) { throw "PowerShell 7 or higher is required for installation" }
-Stop-Service -Force -Name "OBAS Agent Service"; Invoke-WebRequest -Uri "${OPENBAS_URL}/api/agent/package/openbas/windows/${architecture}" -OutFile "openbas-installer.exe"; ./openbas-installer.exe /S ~OPENBAS_URL="${OPENBAS_URL}" ~ACCESS_TOKEN="${OPENBAS_TOKEN}" ~UNSECURED_CERTIFICATE=${OPENBAS_UNSECURED_CERTIFICATE} ~WITH_PROXY=${OPENBAS_WITH_PROXY}; Start-Sleep -Seconds 1.5; rm -force ./openbas-installer.exe;
+echo "Stopping existing openbas agent service..."
+Stop-Service -Force -Name "OBAS Agent Service";
+echo "Downloading and installing OpenBAS Agent..."
+try {
+    Invoke-WebRequest -Uri "${OPENBAS_URL}/api/agent/package/openbas/windows/${architecture}" -OutFile "openbas-installer.exe";
+    ./openbas-installer.exe /S ~OPENBAS_URL="${OPENBAS_URL}" ~ACCESS_TOKEN="${OPENBAS_TOKEN}" ~UNSECURED_CERTIFICATE=${OPENBAS_UNSECURED_CERTIFICATE} ~WITH_PROXY=${OPENBAS_WITH_PROXY};
+    Start-Sleep -Seconds 3;
+    rm -force ./openbas-installer.exe;
+	echo "OpenBAS agent has been successfully installed"
+} catch {
+  	throw "Installation failed : PowerShell 7 or higher is required for installation"
+} finally {
+  	if ($location -like "*C:\Windows\System32*") { cd C:\Windows\System32 }
+}
