35c8da20af58b45e26ff6dbef499c8598a777b46
Gael Leblan <gael.leblan@filigran.io>
2025-01-23 17:53:44 +0100

    [agent] Adding first test

--- src/tests/process/agent_exec_tests.rs
+++ src/tests/process/agent_exec_tests.rs
@@ -4,8 +4,8 @@ use after_test::cleanup;
 #[cleanup(cleanup_after_tests)]
 mod tests {
     use crate::process::agent_exec::command_execution;
-    use std::fs;
     use std::env;
+    use std::fs;
 
     const TEST_AGENT_ID: &str = &"62e1e7a6-79af-47ae-ac4a-8324c2b82197";
     const CLEANUP_ENABLED: bool = true;
@@ -21,10 +21,7 @@ mod tests {
 
     #[test]
     fn test_simple_execution_no_panic() {
-        let result = command_execution(
-            TEST_AGENT_ID,
-            &"echo 'Hello World'",
-        );
+        let result = command_execution(TEST_AGENT_ID, &"echo 'Hello World'");
         assert!(result.is_ok());
     }
 }
